eb1192faaa472f4927a547de3c5115ac7c998110
sepi1996 <jps74@alu.ua.es>
2020-02-29 11:13:13 -0100

    paginas de error y confg separada

--- medium/config.py
+++ medium/config.py
@@ -1,15 +1,19 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SECRET_KEY = config.get('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///medium.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
 
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
+
 
